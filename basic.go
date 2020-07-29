589a44d1dbe3b609675943f7c7c31519c64020f8
向南 <weibo0718@126.com>
2020-07-29 13:46:16 +0800

    first commit

---
+++ basic.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
+
+var (
+	aa = 3
+	bb = true
+	cc = "apple"
+)
+
+func variableZeroValue() {
+	var a int
+	var s string
+	fmt.Printf("%d %q\n", a, s)
+}
+
+func variableInitialValue() {
+	var a, b int = 3, 5
+	var s string = "aaa"
+	fmt.Println(a, b, s)
+}
+
+func variableTypeDeduction() {
+	var a, b, c, d = 3, 5, true, "define"
+	fmt.Println(a, b, c, d)
+}
+
+func variableShorter() {
+	a, b, c, d := 3, 5, true, "define"
+	b = 6
+	fmt.Println(a, b, c, d)
+}
+
+func euler() {
+	fmt.Println(cmplx.Pow(math.E, 1i * math.Pi) + 1)
+	fmt.Printf("%.3f\n", cmplx.Exp(1i * math.Pi) + 1)
+}
+
+func triangle() {
+	var a, b int = 3, 4
+	var c int
+	c = int(math.Sqrt(float64(a*a + b*b)))
+	fmt.Println(c)
+}
+
+func consts() {
+	const (
+		filename = "a.mp4"
+		a, b = 3, 4
+	)
+	var c int
+	c = int(math.Sqrt(a*a + b * b))
+	fmt.Println(filename, c)
+}
+
+func enumerate() {
+	const (
+		cpp = iota
+		java
+		python
+		golang
+		javascript
+	)
+	const (
+		b = 1 << (10 * iota)
+		kb
+		mb
+		gb
+		tb
+		pb
+	)
+	fmt.Println(cpp, java, python, golang, javascript)
+	fmt.Println(b, kb, mb, gb, tb, pb)
+}
+
+func main() {
+	fmt.Println("Hello World!")
+	variableZeroValue()
+	variableInitialValue()
+	variableTypeDeduction()
+	variableShorter()
+	fmt.Println(aa, bb, cc)
+	euler()
+	triangle()
+	consts()
+	enumerate()
+}
---
+++ branch.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func grade(score int) string {
+	g := ""
+	switch {
+	case score < 0 || score > 100:
+		panic(fmt.Sprintf("Wrong score: %d", score))
+	case score < 60:
+		g = "F"
+	case score < 70:
+		g = "D"
+	case score < 80:
+		g = "C"
+	case score < 90:
+		g = "B"
+	case score <= 100:
+		g = "A"
+	default:
+		panic(fmt.Sprintf("Wrong score: %d", score))
+	}
+	return g
+}
+
+func readFile() {
+	const filename = "abc.txt"
+	contents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%s\n", contents)
+	}
+	if contents, err := ioutil.ReadFile(filename); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%s\n", contents)
+	}
+}
+
+func main() {
+	//readFile()
+	fmt.Println(
+		grade(0),
+		grade(45),
+		grade(63),
+		grade(78),
+		grade(85),
+		grade(92),
+		grade(100),
+	)
+}
